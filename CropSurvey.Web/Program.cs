ee973ea43d48b99ccddf381f67ca908ff4a081a2
Mirna Pibernik <mpibernik@croz.net>
2022-05-24 09:45:18 +0200

    Add project files.

---
+++ CropSurvey.Web/Program.cs
@@ -0,0 +1,57 @@
+using CropSurvey.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        connectionString,
+        opt => opt.MigrationsAssembly("CropSurvey.DAL")
+    ));
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services
+    .AddAuthentication()
+    .AddGoogle(googleOptions =>
+        {
+            googleOptions.ClientId = builder.Environment.IsProduction() ? Environment.GetEnvironmentVariable("CLIENT_ID") : builder.Configuration["Authentication:Google:ClientId"];
+            googleOptions.ClientSecret = builder.Environment.IsProduction() ? Environment.GetEnvironmentVariable("CLIENT_SECRET") : builder.Configuration["Authentication:Google:ClientSecret"];
+        });
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services
+    .AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
