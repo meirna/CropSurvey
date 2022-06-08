5291331ce18f61f888fc7b9a739411d44aa6caf8
Mirna Pibernik <mpibernik@croz.net>
2022-06-08 12:08:16 +0200

    Add Survey controller authorization, page Survey/Start

--- CropSurvey.Web/Controllers/SurveyController.cs
+++ CropSurvey.Web/Controllers/SurveyController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CropSurvey.Web.Controllers
 {
+    [Authorize]
     public class SurveyController : Controller
-    {
+    {       
         public IActionResult Index()
         {
             ViewData["completed"] = 0;
+            return View();
+        }
+
+        public IActionResult Start()
+        {
             return View();
         }
     }
