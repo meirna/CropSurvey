e6754d5d31d81ddfbe17e60bfe9c30d88fd91607
Mirna Pibernik <mpibernik@croz.net>
2022-06-08 11:38:53 +0200

    Add page Survey/Index

---
+++ CropSurvey.Web/Controllers/SurveyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CropSurvey.Web.Controllers
+{
+    public class SurveyController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["completed"] = 0;
+            return View();
+        }
+    }
+}
